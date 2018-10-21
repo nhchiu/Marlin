af33e4d9e6b3e42ffb8f30bcbe3d4af8e617b254
nhchiu <nhchiu2009@gmail.com>
2018-10-22 00:23:26 +0800

    Change TEMP_0_PIN due to broken pin on my TRIGORILLA Board.

--- Marlin/src/pins/ramps/pins_RAMPS.h
+++ Marlin/src/pins/ramps/pins_RAMPS.h
@@ -158,7 +158,7 @@
 // Temperature Sensors
 //
 #ifndef TEMP_0_PIN
-  #define TEMP_0_PIN       13   // Analog Input
+  #define TEMP_0_PIN       15   // Analog Input
 #endif
 #ifndef TEMP_1_PIN
   #define TEMP_1_PIN       15   // Analog Input
